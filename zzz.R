50ac536200f1dfbf84286121f74a28ca014bbc98
Brad Barkhouse <brad.barkhouse@gmail.com>
2017-06-20 16:25:52 -0300

    Amend package initialization

--- zzz.R
+++ zzz.R
@@ -1,11 +1,8 @@
-.MySportsFeedsEnv <- new.env()
-.MySportsFeedsEnv$data <- list()
-
 .onLoad <- function(libname, pkgname){
-  if(is.null(.MySportsFeedsEnv$data) == FALSE){
-  	.MySportsFeedsEnv$data <- list(
-    	v1_0_username <- NULL,
-      v1_0_password <- NULL
-    )
-  }
+  .MySportsFeedsEnv <- new.env()
+
+  .MySportsFeedsEnv$data <- list(
+    v1_0_username <- NULL,
+    v1_0_password <- NULL
+  )
 }
